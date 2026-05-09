5e987122c220544434af19545b7328866aa74117
Gabriel Malachias Hangrad <gbl.malachias@gmail.com>
2026-05-09 13:10:47 -0300

    Criação do mysite
    ajustes dos models e criação de um novo maneger.

---
+++ mysite/blog/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return (
+            super(PublishedManager, self)
+            .get_queryset()
+            .filter(status="published")
+        )
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+    publish = models.DateField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def __str__(self) -> str:
+        return self.title
